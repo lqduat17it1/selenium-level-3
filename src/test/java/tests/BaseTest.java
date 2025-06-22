3d1c825baa5c79a19e0b0a07eab2f8a24644b4e2
Duat Le <lqduat99@gmail.com>
2025-06-22 22:36:02 +0700

    add allure report

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,12 +1,16 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.BeforeClass;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class BaseTest {
+
     @BeforeClass
     public void init() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         open("/");
     }
 }
